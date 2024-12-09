b3aa34be4574774f3dde62cd1068c5e7092808d8
replakcan <alper.makcan@gmail.com>
2024-12-09 14:05:38 +0300

    s15d3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,82 @@
 package org.example;
 
 
+import org.example.entity.Employee;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LinkedList<Employee> employeeList = new LinkedList<>();
+        Employee employee1 = new Employee(1,"Didem","Demir");
+        Employee employee2 = new Employee(2,"Begüm","Tokyay");
+        Employee employee3 = new Employee(3,"Ömer", "Durmuş");
+        Employee employee4 = new Employee(4, "Berkay","Eser");
+        Employee employee5 = new Employee(5,"Feyruze","Kekeç");
+        Employee employee6 = new Employee(1,"Didem","Demir");
+
+        employeeList.add(employee1);
+        employeeList.add(employee2);
+        employeeList.add(employee3);
+        employeeList.add(employee4);
+        employeeList.add(employee5);
+        employeeList.add(employee6);
+
+        List<Employee> duplicates = findDuplicates(employeeList);
+        for(Employee employee:duplicates){
+            System.out.println(employee);
+        }
+        System.out.println("**************");
+        Map<Integer,Employee> uniqueMap = findUniques(employeeList);
+        for(Map.Entry<Integer,Employee> entry: uniqueMap.entrySet()){
+            System.out.println(entry.getValue());
+        }
+        System.out.println("**************");
+        List<Employee> uniqueEmployees = removeDuplicates(employeeList);
+        for(Employee employee : uniqueEmployees){
+            System.out.println(employee);
+        }
+
+
+    }
+
+    public static List<Employee> findDuplicates(List<Employee> employees) {
+        Set<Employee> checked = new HashSet<>();
+        List<Employee> duplicates = new ArrayList<>();
+
+        for(Employee employee: employees){
+            if(!checked.add(employee)){
+                duplicates.add(employee);
+            }
+        }
+        return duplicates;
+    }
+    public static Map<Integer,Employee> findUniques(List<Employee> employees){
+        Map<Integer, Employee> uniqueMap = new HashMap<>();
+        Set<Integer> seenIds = new HashSet<>();
+
+        for (Employee employee : employees) {
+            if (employee != null && !seenIds.contains(employee.getId())) {
+                uniqueMap.put(employee.getId(), employee);
+                seenIds.add(employee.getId());
+            }
+        }
+        return uniqueMap;
+    }
+    public static List<Employee> removeDuplicates(List<Employee> employees){
+        Map<Integer,Integer> countMap = new HashMap<>();
+        for (Employee employee : employees) {
+            if (employee != null) {
+                int id = employee.getId();
+                countMap.put(id, countMap.getOrDefault(id, 0) + 1);
+            }
+        }
+        LinkedList<Employee> uniqueList = new LinkedList<>();
+        for (Employee employee : employees) {
+            if (employee != null && countMap.get(employee.getId()) == 1) {
+                uniqueList.add(employee);
+            }
+        }
+        return uniqueList;
     }
 }
---
+++ src/main/java/org/example/entity/Employee.java
@@ -0,0 +1,61 @@
+package org.example.entity;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public Employee(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
